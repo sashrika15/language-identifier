5c48c0398438544288acf40d383e20b8eb7733d5
Aditi Ashish Gawande <68374073+AditiGawande4@users.noreply.github.com>
2022-04-04 10:48:59 +0530

    Initial Commit

---
+++ data_loading.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+def dataLoading(path):
+    data = pd.read_csv(path)
+    data["Language"].value_counts()
+    X = data["Text"]
+    y = data["Language"]
+    return X,y
+
+def labelencoding(y):
+    le = LabelEncoder()
+    y = le.fit_transform(y)
